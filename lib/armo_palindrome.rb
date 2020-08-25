70d141e9eaa96f29ebd0c00590b4ac3aacdd23dd
Ari Moilanen <armo@iki.fi>
2020-08-25 10:03:12 +0300

    Initialize repository

---
+++ lib/armo_palindrome.rb
@@ -0,0 +1,6 @@
+require "armo_palindrome/version"
+
+module ArmoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/armo_palindrome_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class ArmoPalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ArmoPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
